975ac9b08fe0cdff4afa5225ccb3e1171a1fca4e
110628@st-maartenscollege.nl <110628@st-maartenscollege.nl>
2022-04-13 09:21:44 +0200

    get all products (database)

--- Festivlakte/Controllers/HomeController.cs
+++ Festivlakte/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MySql.Data;
 using Festivlakte.Database;
+using Festivlakte.Controllers.Databases;
 
 namespace Festivlakte.Controllers
 {
@@ -88,5 +89,31 @@ namespace Festivlakte.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public List<Product> GetAllProducts ()
+        {
+
+            //alle producten ophalen uit de database
+            var rows = DatabaseConnector.GetRows("select * from product");
+
+            //lijst maken om allee producten in te stoppen
+            List<Product> products = new List<Product>();
+
+            foreach (var row in rows)
+            {
+                //Voor elke rij maken we nu een product
+                Product p = new Product();
+                p.Naam = row["naam"].ToString();
+                p.Prijs = row["prijs"].ToString ();
+                p.Beschikbaarheid = Convert.ToInt32(row["beschikbaarheid"]);
+                p.Id = Convert.ToInt32(row["id"]);
+
+                //en dat product voegen we toe aan de lijst met producten
+                products.Add(p);
+            }
+
+            return products;
+        }
+
     }
 }
